1d16fcdb6b27cc857d0c888140d3c4c3e036d578
ptrckbnck <ptrckbnck@gmail.com>
2018-10-03 17:43:44 +0200

    changed text of about page

--- src/main/java/de/unifrankfurt/dbis/GUI/AboutPage.java
+++ src/main/java/de/unifrankfurt/dbis/GUI/AboutPage.java
@@ -24,19 +24,19 @@ public class AboutPage {
     public static void setText(VBox vbox) {
 
         String text = "Dieses Programm wurde zur Unterstützung von Datenbanken-Vorlesungen an der Goethe Universität " +
-                "Frankfurt am GUIApp entwickelt. Es soll das Einreichen von SQL-bezogenen Aufgaben erleichtern.";
-        String text2 = "Sollten Sie Fragen oder Anregungen haben, wenden sie sich bitte an die Betreuer Ihrer Übung.";
-        String text31 = "Alternativ können Sie Bug-Reporte direkt auf";
+                "Frankfurt entwickelt. Es soll das Einreichen von SQL-bezogenen Aufgaben erleichtern.";
+        String text2 = "Sollten Sie Fragen haben, lesen Sie bitte die Nutzer-Dokumentation oder wenden Sie sich an die Betreuer Ihrer Übung.";
+        String text31 = "Alternativ können Sie Bug-Reporte oder Vorschläge direkt auf";
         String text33 = "einreichen.";
         String text5 = "Viel Erfolg beim Nutzen der Software.";
         Hyperlink hyperlink = new Hyperlink("SQLChecker");
-        hyperlink.setOnAction(event -> {
-            GUIApp.getHostServicesStatic().showDocument("https://github.com/ptrckbnck/SQLChecker");
-        });
+        hyperlink.setOnAction(event -> GUIApp.getHostServicesStatic()
+                .showDocument("https://github.com/ptrckbnck/SQLChecker"));
 
         List<Node> nodes1 = List.of(text.split(" ")).stream().map(Label::new).collect(Collectors.toList());
         List<Node> nodes2 = List.of(text2.split(" ")).stream().map(Label::new).collect(Collectors.toList());
         List<Node> nodes3 = List.of(text31.split(" ")).stream().map(Label::new).collect(Collectors.toList());
+        List<Node> nodes5 = List.of(text5.split(" ")).stream().map(Label::new).collect(Collectors.toList());
         nodes3.add(hyperlink);
         nodes3.addAll(List.of(text33.split(" ")).stream().map(Label::new).collect(Collectors.toList()));
 
@@ -44,6 +44,7 @@ public class AboutPage {
         vbox.getChildren().add(initFlowPane(nodes1));
         vbox.getChildren().add(initFlowPane(nodes2));
         vbox.getChildren().add(initFlowPane(nodes3));
+        vbox.getChildren().add(initFlowPane(nodes5));
 
 
     }
